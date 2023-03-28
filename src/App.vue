da4275d60ac6c4320a5b4327214661cfa9e7d161
starry <>
2023-03-28 19:26:07 +0800

    feat: fin

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
     <div class="grid grid-cols-12 grid-rows-12 max-h-screen  gap-4">
 
-      <div class="grid col-span-12 row-span-1 border-2   border-green-300">
+      <div class="grid col-span-12 row-span-1 border-2  border-green-300">
         <div class="navbar bg-base-100">
           <div class="flex-auto">
             <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
@@ -25,11 +25,17 @@
               </div>
             </div>
           </div>
-          <div class="flex-none">
-            <ul class="menu menu-horizontal px-1">
-              <button class="btn btn-accent" @click="toggleTheme">Toggle Theme</button>
-            </ul>
+          <div class="flex-none form-control mr-5">
+            <label class="label cursor-pointer">
+              <span class="label-text mr-3">Only Show Auth</span>
+              <input type="checkbox" class="toggle" v-model="onlyShowAuth" />
+            </label>
           </div>
+          <label class="flex-none swap swap-rotate mr-5">
+            <input type="checkbox" />
+            <svg  @click="toggleTheme" class="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+            <svg  @click="toggleTheme" class="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
+          </label>
         </div>
       </div>
 
@@ -52,7 +58,7 @@
             <div class="chat-bubble chat-bubble-success" v-html="item.comment_content">
             </div>
           </div>
-          <div class="chat chat-end">
+          <div class="chat chat-end" v-if="item?.replies ?? false">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full border-2">
                 <img src="./assets/logo.png" />
@@ -69,7 +75,8 @@
       </div>
 
 
-      <div class="col-span-1 row-span-11"></div>
+      <div class="col-span-1 row-span-10">
+      </div>
 
 
       <div class="col-span-2 row-span-1 border-2  grid place-items-center ">
@@ -99,6 +106,10 @@
           </ul>
         </div>
       </div>
+      <div class="row-span-1">
+        <button class="btn btn-primary" @click="getCommentList">NEXT</button>
+      </div>
+
     </div>
 
   </div>
@@ -116,8 +127,6 @@ export default {
     this.book.curBookName = localStorageUtil.getCurBookNameFromConst()
     this.book.curBookId = localStorageUtil.getCurBookIdFromConst()
     this.articleParams.cid = this.book.curBookId
-    // this.article.curId = localStorageUtil.getCurArticleIdFromConst()
-    // this.article.curTitle = localStorageUtil.getCurArticleTitleFromConst()
     this.getArticleList(true)
   },
   data() {
@@ -126,6 +135,7 @@ export default {
         isDark: true,
         curTheme: 'halloween',
       },
+      onlyShowAuth: true,
       book: {
         bookId: '',
         bookName: '',
@@ -277,7 +287,16 @@ export default {
     getCommentList() {
       getCommentApi(this.commentParams).then(resp => {
         this.commentList = resp.data.data.list
+        this.commentParams.prev = this.commentList[this.commentList.length - 1].score
+        if (this.onlyShowAuth) {
           this.commentList = this.commentList.filter(item => item.replies !== undefined && item.replies.length !== 0)
+          console.log(true)
+        } else {
+          console.log(false)
+        }
+        if (this.commentList.length === 0) {
+          this.openAlert("当前页无评论")
+        }
       })
     }
   }
